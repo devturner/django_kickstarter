f12beb0c3051d08ca9cc2caee72bd41d8854b326
Ben Hurst <BHurst8@gmail.com>
2018-09-25 13:57:59 -0700

    Views/Urls linked in

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -8,3 +8,4 @@ python3 manage.py makemigrations --noinput && \
 python3 manage.py migrate --noinput && \
 python3 manage.py collectstatic --noinput && \
 python3 manage.py runserver 0.0.0.0:8000
+
--- web/project_data/views.py
+++ web/project_data/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.core.paginator import Paginator
 from .models import Kickstarter
 
 
 def kickstarter_list_view(request):
     kickstarters = get_list_or_404(Kickstarter)
+    paginator = Paginator(kickstarters, 20)
+    page = request.GET.get('page')
+    kickstarters = paginator.get_page(page)
     context = {
         'kickstarters': kickstarters
     }
@@ -12,7 +16,7 @@ def kickstarter_list_view(request):
 
 
 def kickstarter_detail_view(request, pk=None):
-    kickstarter = get_object_or_404(Kickstarter)
+    kickstarter = get_object_or_404(Kickstarter, id=pk)
     context = {
         'kickstarter': kickstarter
     }
